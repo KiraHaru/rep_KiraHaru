1b87086bdb04f8e560c0757bc69f3551754b9a20
Poll <davp1808@gmail.com>
2024-11-19 10:21:28 +0300

    task_Lab_5

--- Lab_5.py
+++ Lab_5.py
@@ -1,24 +1,23 @@
 import random
-import time
 import keyboard
 
 
-def start_of_the_game():
+def start():
     print(
         "Добро пожаловать в игру! \n"
         "Правила: В начале игры мы случайным образом определим начальное количество камней в куче.\n"
         "Далее игроки будут по очереди выбирать какое количество камней они хотят убрать из кучи (1 - 3 камня).\n"
         "Победителем считается игрок, после окончания хода которого, в куче остался один камень.\n"
-        "Если вы хотите сыграть, нажмите Y. Для выхода, нажмите N.")
+        "Если вы хотите сыграть, нажмите 1. Для выхода, нажмите 0.")
 
     while True:
-        if keyboard.is_pressed('Y'):
+        if keyboard.is_pressed('1'):
             return True
-        if keyboard.is_pressed('N'):
+        if keyboard.is_pressed('0'):
             return False
 
 
-def check_for_save_data(input_data):
+def check(input_data):
     for char in input_data:
         if char not in "012345678":
             return False
@@ -27,50 +26,47 @@ def check_for_save_data(input_data):
         return True
 
 
-def get_ai_step(number_of_stones):
-    if number_of_stones <= 4:
-        return 1, abs(1 - number_of_stones)
+def get_ai_step(num_stones):
+    if num_stones <= 4:
+        return 1, abs(1 - num_stones)
     ai_step = random.randint(1, 3)
-    return number_of_stones - ai_step, ai_step
+    return num_stones - ai_step, ai_step
 
 
-if start_of_the_game():
+if start():
 
-    number_of_stones = random.randint(4, 30)
+    num_stones = random.randint(4, 30)
     win = "undefind"
 
     while win == "undefind":
-        print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
+        print(f"\nТекущее значение камней в куче: {num_stones}\n")
         print("Ваш ход\n"
               "Введите число камней, которое хотите убрать из кучи (1 - 3 камня): ")
 
         input_data = input()
 
-        if check_for_save_data(input_data):
-            user_stone_choose = int(input_data)
-            number_of_stones = number_of_stones - user_stone_choose
+        if check(input_data):
+            user_1 = int(input_data)
+            num_stones = num_stones - user_1
 
-            if number_of_stones == 1:
+            if num_stones == 1:
                 print("\nВы победили!")
                 win = "User_win"
-            elif number_of_stones < 1:
+            elif num_stones < 1:
                 print("Подумайте еще раз")
-            elif number_of_stones > 1:
-                print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
+            elif num_stones > 1:
+                print(f"\nТекущее значение камней в куче: {num_stones}\n")
                 print("Ход другого игрока ...")
+                num_stones, user_2 = get_ai_step(num_stones)
 
-                time.sleep(2)
-                number_of_stones, ai_stone_choose = get_ai_step(number_of_stones)
-
-                if ai_stone_choose == 1:
-                    print(f"Другой игрок убрал {ai_stone_choose} камень из кучи")
+                if user_2 == 1:
+                    print(f"Другой игрок убрал {user_2} камень из кучи")
                 else:
-                    print(f"Другой игрок убрал {ai_stone_choose} камня из кучи")
+                    print(f"Другой игрок убрал {user_2} камня из кучи")
 
-                if number_of_stones == 1:
-                    print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
+                if num_stones == 1:
+                    print(f"\nТекущее значение камней в куче: {num_stones}\n")
                     print("Вы проиграли. Желаем удачи в следующий раз!")
-                    win = "ai_win"
-
+                    win = "user_2_win"
         else:
             print("Неверные данные, попробуйте еще раз")
