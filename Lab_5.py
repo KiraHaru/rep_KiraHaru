9cc71e1c5833ff1b2e970c5d5f89fd162f2bc5c9
Poll <davp1808@gmail.com>
2024-11-30 14:27:23 +0300

    Revert "task_Lab_5"
    This reverts commit 3d56e8d366eabab8c09e5341db2011a8ec0f1df9.
    
    # Conflicts:
    #	Lab_5.py

--- Lab_5.py
+++ Lab_5.py
@@ -1,6 +1,4 @@
 import random
-import time
-import keyboard
 
 
 def start_of_the_game():
@@ -8,14 +6,7 @@ def start_of_the_game():
         "Добро пожаловать в игру! \n"
         "Правила: В начале игры мы случайным образом определим начальное количество камней в куче.\n"
         "Далее игроки будут по очереди выбирать какое количество камней они хотят убрать из кучи (1 - 3 камня).\n"
-        "Победителем считается игрок, после окончания хода которого, в куче остался один камень.\n"
-        "Если вы хотите сыграть, нажмите Y. Для выхода, нажмите N.")
-
-    while True:
-        if keyboard.is_pressed('Y'):
-            return True
-        if keyboard.is_pressed('N'):
-            return False
+        "Победителем считается игрок, после окончания хода которого, в куче остался один камень.\n")
 
 
 def check_for_save_data(input_data):
@@ -38,7 +29,6 @@ if start_of_the_game():
 
     number_of_stones = random.randint(4, 30)
     win = "undefind"
-
     while win == "undefind":
         print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
         print("Ваш ход\n"
@@ -59,7 +49,6 @@ if start_of_the_game():
                 print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
                 print("Ход другого игрока ...")
 
-                time.sleep(2)
                 number_of_stones, ai_stone_choose = get_ai_step(number_of_stones)
 
                 if ai_stone_choose == 1:
@@ -71,6 +60,5 @@ if start_of_the_game():
                     print(f"\nТекущее значение камней в куче: {number_of_stones}\n")
                     print("Вы проиграли. Желаем удачи в следующий раз!")
                     win = "ai_win"
-
         else:
             print("Неверные данные, попробуйте еще раз")
